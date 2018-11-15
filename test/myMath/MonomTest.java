930836b36dd83af7577135c877b86514177e312d
orelrahum <44848947+orelrahum@users.noreply.github.com>
2018-11-15 20:46:52 +0200

    new source folder with test

---
+++ test/myMath/MonomTest.java
@@ -0,0 +1,54 @@
+package myMath;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MonomTest {
+
+	@Test
+	void testF() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDerivative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultyply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultyplyans() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsZero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddForPolynom() {
+		fail("Not yet implemented");
+	}
+
+}
